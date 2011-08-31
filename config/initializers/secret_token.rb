817833cb9a23f07c40b76b46f93ebb7cb88a29d5
RayHightower <rth@wisdomgroup.com>
2011-08-31 11:53:25 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Exp31app::Application.config.secret_token = '9c02129828b8173e21b8d38249207e85c037b60f753cdee3c472d5bb39ba775356b64e61c4d845de44ee1dd50de70b4cb50c72e56be84fdf152922505d61eabc'
